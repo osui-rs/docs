3ff6951557d42b7ce21a19b7c1238df94032f4e7
Leo dev <klesti.selimay@icloud.com>
2024-11-26 19:37:06 +0100

    docs

---
+++ pages/_app.jsx
@@ -0,0 +1,5 @@
+import './fonts.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ theme.config.jsx
@@ -0,0 +1,20 @@
+export default {
+  logo: (
+    <>
+      <span>
+        <img src="/osuir.png" alt="Logo" width="35" height="35" />
+      </span>
+      <h1 style={{
+        fontFamily: 'Researcher',
+        fontWeight: 700,
+        marginLeft: "5px",
+        marginTop: "4px",
+      }}>
+        OSUI
+      </h1>
+    </>
+  ),
+  project: {
+    link: 'https://github.com/shuding/nextra'
+  },
+};
